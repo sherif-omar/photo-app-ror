96795935f1e9f4923c9d11f60804763504c662df
sherifhassan <cxt@live.ca>
2016-07-31 23:59:29 +0000

    Add CreditCard payment

---
+++ app/models/user.rb
@@ -0,0 +1,8 @@
+class User < ActiveRecord::Base
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable and :omniauthable
+  devise :database_authenticatable, :registerable,:confirmable,
+         :recoverable, :rememberable, :trackable, :validatable
+  has_one :payment
+  accepts_nested_attributes_for :payment
+end
