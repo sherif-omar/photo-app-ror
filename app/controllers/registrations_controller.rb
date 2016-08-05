0c1bb8866346ae8e9d7d1f8f50ca8119fe7f2034
sherifhassan <cxt@live.ca>
2016-08-05 12:25:27 +0000

    payment stripe

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -1,24 +1,27 @@
 class RegistrationsController < Devise::RegistrationsController
-
   def create
     build_resource(sign_up_params)
+    
     resource.class.transaction do
       resource.save
       yield resource if block_given?
       if resource.persisted?
-        @payment = Payment.new({ email: params["user"]["email"],
+        @payment = Payment.new({ email: params["user"]["email"], 
           token: params[:payment]["token"], user_id: resource.id })
+        
         flash[:error] = "Please check registration errors" unless @payment.valid?
+        
         begin
           @payment.process_payment
           @payment.save
-        rescue Exception => e
+        rescue Exception => e 
           flash[:error] = e.message
+          
           resource.destroy
           puts 'Payment failed'
           render :new and return
         end
-
+        
         if resource.active_for_authentication?
           set_flash_message :notice, :signed_up if is_flashing_format?
           sign_up(resource_name, resource)
@@ -35,10 +38,10 @@ class RegistrationsController < Devise::RegistrationsController
       end
     end
   end
-
+  
   protected
+  
   def configure_permitted_parameters
     devise_parameter_sanitizer.for(:sign_up).push(:payment)
   end
-
 end
